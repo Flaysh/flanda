59df7db62b9262dd88c91b28ad92820661859e2c
Flaysh MB <Flaysh@moonwiz.io>
2022-11-09 19:16:17 +0200

    yea

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 import {Route, Routes} from "react-router";
+import Home from "./components/Home/Home";
+import {Container, createTheme, ThemeProvider} from "@mui/material";
+import Box from "@mui/material/Box";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="flanda">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<Feed />} />
-          <Route path="/settings" element={<House />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </header>
-    </div>
-  );
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#144e4d',
+            }, secondary: {
+                main: '#33a688',
+            }, background: {
+                default: '#f9f7f1', paper: '#ffffff',
+            }, text: {
+                main: '#151515'
+            }
+        }, shape: {
+            borderRadius: 20,
+        },
+    })
+    return (<Box className="App">
+        <ThemeProvider theme={theme}>
+            <Container fixed>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<Feed/>}/>
+                    <Route path="/settings" element={<House/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </Container>
+        </ThemeProvider>
+    </Box>);
 }
 
 export default App;
